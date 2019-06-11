3fc2728d6688e5855f18bf70413ffff38c9faa98
Huiyan Zhang <nicolezhang@brandeis.edu>
2019-06-11 10:40:36 -0400

    notes added

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//add here to create the logic of new page, new page in views
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
