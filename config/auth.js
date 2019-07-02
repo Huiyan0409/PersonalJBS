a1488cf4a4e698fd078279d92852769d86e01a16
Huiyan Zhang <nicolezhang@brandeis.edu>
2019-07-02 14:06:38 -0400

    client

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,8 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
+        //'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
+        'clientID': '795278460451-c67lrnsbchqm3j01n2l7s0v7csn8s5g1.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
         //'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
         'callbackURL'   : 'https://friendly-bunnyhug-64182.herokuapp.com/login/authorized'
